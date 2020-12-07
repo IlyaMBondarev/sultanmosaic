5f04e075be1d3116af2f572748266002a8bb2313
IlyaMBondarev <60775995+IlyaMBondarev@users.noreply.github.com>
2020-12-07 15:29:20 +0300

    Delete chiefsPhotos.js

--- js/chiefsPhotos.js
+++
@@ -1,8 +0,0 @@
-let chiefsPhotos = document.querySelectorAll('.chiefs__image-form');
-
-chiefsPhotos.forEach(photo => {
-    let src = photo.querySelector('.chiefs__image').src;
-    let back = photo.querySelector('.chiefs__image-bg > div');
-    back.style.background = `url(${src}) no-repeat center`;
-    back.style.backgroundSize = 'cover';
-})
